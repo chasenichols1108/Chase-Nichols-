b9c163d7c9b077c8d67f004f7aabf205e20dc321
chasenichols1108 <chasenichols1108@gmail.com>
2026-09-06 22:28:51 -0400

    Add files via upload

---
+++ Welcome to SDEV 120 Class.py
@@ -0,0 +1,7 @@
+Python 3.14.7 (tags/v3.14.7:823f032, Aug  5 2026, 10:51:32) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/chase/AppData/Local/Programs/Python/Python314/Welcome to SDEV 120 Class.py
+Welcome to SDEV 120 class
+Enter your name Chase
+Hello Chase
