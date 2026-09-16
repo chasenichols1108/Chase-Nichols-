743fa1ef460c0abc989f9262936f833033753245
chasenichols1108 <chasenichols1108@gmail.com>
2026-09-16 19:12:51 -0400

    Add files via upload

---
+++ mod 4 python test score.py
@@ -0,0 +1,12 @@
+Python 3.14.7 (tags/v3.14.7:823f032, Aug  5 2026, 10:51:32) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+======== RESTART: C:\Users\chase\OneDrive\cmp logic\final_grade mod 4.py =======
+please enter your name: David
+Please enter your final grade: 95
+Hello David, you passed this class
+>>> 
+======== RESTART: C:\Users\chase\OneDrive\cmp logic\final_grade mod 4.py =======
+please enter your name: David
+Please enter your final grade: 50
+Hello David, you failed this class
